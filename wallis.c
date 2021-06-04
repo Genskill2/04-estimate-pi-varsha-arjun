7e36875b23fd6e269a79257546e345f480322f31
varsha-arjun <84783204+varsha-arjun@users.noreply.github.com>
2021-06-04 13:38:32 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -4,7 +4,16 @@
 #include <math.h>
 
 float wallis_pi(int);
-
+{float pi=1.0;
+for (int i=1;i<n; i++)
+{
+int m;
+m=4.0*i*i;
+pi=(float) ((pi*m)/(m-1));
+}
+pi=pi *2.0;
+return pi;
+}
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
